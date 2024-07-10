be40187a1be476e9b151605e28f725e0e25e2f18
TusharW4ni <reachtusharwani@gmail.com>
2024-07-10 10:14:34 -0500

    removed unncessary lines

--- atc-patient-data/server/middleware/auth.ts
+++ atc-patient-data/server/middleware/auth.ts
@@ -8,144 +8,30 @@ const runtime = useRuntimeConfig();
 export default defineEventHandler(async (event) => {
   event.context.prisma = prismaClient;
   const token = getCookie(event, "token") || "";
-  // not logged in but trying to
   if (!token && !event.node.req.url?.includes("/api/auth/callback")) {
     await sendRedirect(event, loginRedirectUrl());
-    // console.log("after redirect", Date.now().toString());
   } else {
-    // theoretically logged in
-    // console.log("inside of else", Date.now().toString());
     if (token) {
-      // console.log("token", token);
       try {
         const claims = jwt.verify(
           token,
           fs.readFileSync(process.cwd() + "/cert-dev.pem")
         );
-        // console.log("claims", claims);
         event.context.claims = claims;
-        // event.context.user = await event.context.client.user.findFirst({
+        // event.context.currUser = await event.context.prisma.user.findFirst({
         //   where: { email: claims.email },
-        //   include: {
-        //     Pages: {
-        //       select: {
-        //         cuid: true,
-        //       },
-        //     },
-        //     Family: {
-        //       select: {
-        //         stripe_account_id: true,
-        //         Pages: {
-        //           select: {
-        //             cuid: true,
-        //             status: true,
-        //           },
-        //         },
-        //       },
-        //     },
-        //   },
         // });
-        // if (!event.context.user) {
+        // console.log("currUser", event.context.currUser);
+        // if (!event.context.currUser) {
         //   console.error(`${claims.email} not found`);
-        //   setCookie(event, "cvtoken", "");
-        //   setCookie(event, "cvuser", "");
-        //   return await sendRedirect(event, logoutRedirectUrl(cvtoken)); // todo: add error message after failed log in attempt
-        //   return await sendRedirect(event, loginRedirectUrl());
-        // }
-        // // include pages ids to check if that's the family's page.
-        // setCookie(event, "cvuser", JSON.stringify(event.context.user));
-
-        // // check if the family has a stripe account and onboarding them with stripe if not
-        // if (event.context.user?.user_role == "family") {
-        //   try {
-        //     if (event.context.user?.Family?.stripe_account_id == undefined) {
-        //       // todo: fill with useable address type done
-        //       //todo: add support_address
-        //       // we can prefil some customer stuff with salesforce one day
-        //       const newStripeAccount = await stripe.accounts.create({
-        //         business_profile: {
-        //           //industry: "Charities or social service organizations",
-        //           product_description:
-        //             "This is Carson's Village account used for donations on a family page",
-        //           mcc: "8398",
-        //           support_email: "jason@carsonsvillage.org",
-        //           support_phone: "(877) 789-0722",
-        //           support_url: "carsonsvillage.org",
-        //           name: "Carson's Village",
-        //           url:
-        //             "https://pages.carsonsvillage.org/PageList/" +
-        //             event.context.user?.familyCuid,
-        //           /*support_address: {
-        //                      city : 'Dallas',
-        //                      country : 'US',
-        //                      line1 : '2904 Floyd Street',
-        //                      line2 : null,
-        //                      postal_code : 75204,
-        //                      state: 'TX'
-        //                   }*/
-        //         },
-        //         settings: {
-        //           payments: {
-        //             statement_descriptor: "Carsons Village",
-        //           },
-        //           card_payments: {
-        //             statement_descriptor_prefix: "Carsons V",
-        //           },
-        //         },
-        //         type: "standard",
-        //         email: event.context.user.email,
-        //       });
-
-        //       // Adds the stripe_accounnt_id to the family, but the family is still suspended from creating pages and thus preventing stray donations.
-        //       // Too extreme? Maybe we need to elaborate further to allow families to create pages that do not recieve donations.
-        //       const transaction = await event.context.client.$transaction([
-        //         event.context.client.family.update({
-        //           where: { cuid: event.context.user.familyCuid },
-        //           data: { stripe_account_id: newStripeAccount.id },
-        //         }),
-        //         event.context.client.page.updateMany({
-        //           where: {
-        //             familyCuid: event.context.user.familyCuid as string,
-        //           },
-        //           data: { status: "Family Stripe Account needs onboarding" },
-        //         }),
-        //       ]);
-        //       const stripeAccountId = newStripeAccount.id;
-
-        //       if (stripeAccountId) {
-        //         const accountLink = await stripe.accountLinks.create({
-        //           account: stripeAccountId,
-        //           refresh_url: `${runtime.BASEURL}`,
-        //           return_url: `${runtime.BASEURL}/api/complete_onboarding/?stripe_account_id=${stripeAccountId}`,
-        //           type: "account_onboarding",
-        //         });
-
-        //         return await sendRedirect(event, accountLink.url);
-        //       }
-        //     } else {
-        //       const id = event.context.user?.Family?.stripe_account_id;
-        //       const stripeAccountFull = await stripe.accounts.retrieve(
-        //         id as string
-        //       );
-        //       //console.log(stripeAccountFull)
-        //       // if the user backed out of the onboard, they will be redirected back to the onboard
-        //       if (!stripeAccountFull.details_submitted) {
-        //         const accountLink = await stripe.accountLinks.create({
-        //           account: id,
-        //           refresh_url: `${runtime.BASEURL}`,
-        //           return_url: `${runtime.BASEURL}/api/complete_onboarding/?stripe_account_id=${id}`,
-        //           type: "account_onboarding",
-        //         });
-        //         return await sendRedirect(event, accountLink.url);
-        //       }
-        //     }
-        //   } catch (e) {
-        //     console.error(e);
-        //     setCookie(event, "cvtoken", "");
-        //     setCookie(event, "cvuser", "");
-
-        //     return await sendRedirect(event, loginRedirectUrl());
-        //   }
+        //   setCookie(event, "token", "");
+        //   // setCookie(event, "cvuser", "");
+        //   return await fetch("http://localhost:3000/api/auth/logout", {
+        //     method: "GET",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //     },
+        //   });
         // }
       } catch (e) {
         console.error(e);
