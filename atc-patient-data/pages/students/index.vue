195cc25a69b0b039e527260688b77d7aebfe17a3
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2024-07-12 12:28:37 -0500

    !Students Page not correctly added to commit

--- atc-patient-data/pages/students/index.vue
+++ atc-patient-data/pages/students/index.vue
@@ -1,11 +1,100 @@
+<script setup>
+
+const employeeId = 1;
+const employeeRole = 'Admin'; // update to get if the users is admin - TESTING - TESTING - TESTING
+
+const studentId = null;
+const studentFirst = '';
+const studentLast = '';
+
+const { data: allUsers } = await useFetch('/api/user/StudentSearchAll?employeeId=1'); // update to be the to have correct query parameters - TESTING - TESTING - TESTING
+
+function createStudentClick() {
+    window.location.href = "http://localhost:3000/students"; // update to go to create student - TESTING - TESTING - TESTING
+}
+
+function viewStudentClick(id) {
+    window.location.href = "http://localhost:3000/students"; // update to go to selected student - TESTING - TESTING - TESTING
+}
+
+function onSubmit(){
+
+}
+
+</script>
+
+
+
 <template>
-  <div>Hello from students/index.vue</div>
+    <!-- create user student button -->
+    <div v-if="employeeRole=='Admin'">
+      <button
+      title="Create Student"
+      @click="createStudentClick()"
+      >
+        Create Student
+      </button>
+    </div>
+  
+    <!-- search by id -->
+    <div v-if="studentFirst=='' && studentLast==''">
+      <!-- form -->
+      <form class="search-form" @submit.prevent="onSubmit">
+        <h3>Search Students by ID</h3>
 
-  //create user student button
+        <label for="id">ID</label>
+        <input id="id" v-model.trim="studentId">
 
-  //search by id
+        <button type="submit" title="Search">
+          Search
+        </button>
+      </form>
 
-  //search by name
+      <!-- StudentSearchId api -->
 
-  //display all users
-</template>
+      <!-- display result (button) -->
+    </div>
+  
+    <!-- search by name -->
+    <div v-if="studentId==null">
+      <!-- form --> 
+      <form class="search-form" @submit.prevent="onSubmit">
+        <h3>Search Students by Name</h3>
+
+        <label for="first-name">First Name</label>
+        <input id="first-name" v-model.trim="studentFirst">
+
+        <label for="last-name">First Name</label>
+        <input id="last-name" v-model.trim="studentLast">
+
+        <button type="submit" title="Search">
+          Search
+        </button>
+      </form>
+
+      <!-- StudentSearchName api -->
+
+      <!-- display result (button) -->
+    </div>
+  
+    <!-- display all users -->
+    <div v-if="studentId==null && studentFirst=='' && studentLast==''">
+      <h3>All Students</h3>
+      <!-- StudentSearchAll api -->
+      <!-- display result (button) -->
+      <div class="search-results">
+        <ul>
+            <li v-for="user in allUsers" :key="user.id">
+              <button
+                title="Select Student"
+                @click="viewStudentClick(user.id)"
+              >
+                {{ user.id }} {{ user.firstName }} {{ user.lastName }} {{ user.StudentProfile.dob }} <!-- {{ user.StudentProfile.Sessions }} display num essions? -->
+              </button>
+            </li>
+        </ul>
+      </div>
+    </div>
+  
+  </template>
+  
