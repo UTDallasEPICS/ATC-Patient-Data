b3b2951f1feede7c28b6729c1d94635069096e92
TusharW4ni <reachtusharwani@gmail.com>
2024-07-16 14:30:12 -0500

    added PrismaClient type

--- atc-patient-data/server/api/user/create/student.post.ts
+++ atc-patient-data/server/api/user/create/student.post.ts
@@ -1,3 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
 export default defineEventHandler(async (event) => {
   const {
     firstName,
@@ -8,28 +10,30 @@ export default defineEventHandler(async (event) => {
     assignedEmployee,
   } = await readBody(event);
 
-  const user = await event.context.prisma.$transaction(async (p) => {
-    const newUser = await p.user.create({
-      data: {
-        firstName,
-        lastName,
-        email,
-        phoneNumber: String(phoneNumber),
-      },
-    });
+  const user = await event.context.prisma.$transaction(
+    async (p: PrismaClient) => {
+      const newUser = await p.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          phoneNumber: String(phoneNumber),
+        },
+      });
 
-    const convertedDateOfBirth = new Date(dateOfBirth);
+      const convertedDateOfBirth = new Date(dateOfBirth);
 
-    const studentProfile = await p.studentProfile.create({
-      data: {
-        dob: convertedDateOfBirth,
-        assignedEmployeeId: assignedEmployee.id,
-        userId: newUser.id,
-      },
-    });
+      const studentProfile = await p.studentProfile.create({
+        data: {
+          dob: convertedDateOfBirth,
+          assignedEmployeeId: assignedEmployee.id,
+          userId: newUser.id,
+        },
+      });
 
-    return { ...newUser, studentProfile };
-  });
+      return { ...newUser, studentProfile };
+    }
+  );
 
   return {
     statusCode: 201,
