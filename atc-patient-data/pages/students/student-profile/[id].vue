c5bc491eaeadc724dd516e8e605ef11f2ccced3a
TusharW4ni <reachtusharwani@gmail.com>
2024-07-17 17:39:37 -0500

    Added different buttons based on which tab is selected

--- atc-patient-data/pages/students/student-profile/[id].vue
+++ atc-patient-data/pages/students/student-profile/[id].vue
@@ -36,12 +36,28 @@ function toggleOpenProgram() {
       <button
         class="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-t-lg"
         @click="toggleOpenSession"
+        v-if="openSession"
+      >
+        Sessions
+      </button>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-t-lg"
+        @click="toggleOpenSession"
+        v-if="!openSession"
       >
         Sessions
       </button>
       <button
         class="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-t-lg"
         @click="toggleOpenProgram"
+        v-if="openProgram"
+      >
+        Program
+      </button>
+      <button
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-t-lg"
+        @click="toggleOpenProgram"
+        v-if="!openProgram"
       >
         Program
       </button>
