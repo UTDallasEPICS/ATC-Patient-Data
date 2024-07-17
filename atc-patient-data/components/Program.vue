02f7f2efd56c50e36b35ec8e73a4e515147af601
TusharW4ni <reachtusharwani@gmail.com>
2024-07-17 14:16:50 -0500

    basic code in two components

---
+++ atc-patient-data/components/Program.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+<div>hello from program</div>
+</template>
---
+++ atc-patient-data/components/Sessions.vue
@@ -0,0 +1,7 @@
+<script setup>
+
+</script>
+
+<template>
+<div>hello from sessions</div>
+</template>
