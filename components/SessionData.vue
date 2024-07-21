380ab2821e4e94ab10d7b06b23b99b11ed9183f1
TusharW4ni <reachtusharwani@gmail.com>
2024-07-21 12:13:10 -0500

    tried to get data uncessful and unsure whats wrong

--- components/SessionData.vue
+++ components/SessionData.vue
@@ -1,64 +1,62 @@
 <script setup>
-import Checkbox from 'primevue/checkbox';
+import { computed, defineProps, ref, watch } from "vue";
 
 const props = defineProps({
-  //id: Int,  //? needed if saving inside this component
   type: String,
   arrayCount: Number,
-  //save: Boolean //? needed if saving inside this component
 });
 
-let verifiedCount;
-if (props.type == "TRIAL" || props.type == "COUNT") {
-  verifiedCount = 1;
-}
-else{
-  verifiedCount = props.arrayCount;
-}
+const verifiedCount =
+  props.type === "TRIAL" || props.type === "COUNT" ? 1 : props.arrayCount;
 
-const data = ref(Array(verifiedCount));
+const data = ref(Array(verifiedCount).fill(""));
 
-watch([data], () => {
-  // call the save api with current data
+const dataString = computed(() => data.value.join(","));
+
+watch(verifiedCount, () => {
+  console.log("Data String:", dataString.value);
 });
-
 </script>
 
-
-
 <template>
-  <div class="relative grid items-center grid-cols-[repeat(auto-fit,minmax(100px,2fr))] bg-gray-300">
-  <div v-for="i in verifiedCount" class="flex justify-center">
-    <div v-if="type === 'TRIAL' || type === 'TRIAL_ARRAY'">
-      <label v-if="verifiedCount==1" class="flex justify-center">Trial</label>
-      <label v-else class="flex justify-center">Trial {{ i }}</label>
-      <div class="flex justify-center">
-        <div class="px-2 scale-125">
+  <div class="flex flex-col space-y-2">
+    <div
+      v-for="i in verifiedCount"
+      :key="i"
+      class="flex items-center justify-center"
+    >
+      <div v-if="type === 'TRIAL' || type === 'TRIAL_ARRAY'" class="space-y-2">
+        <label v-if="verifiedCount === 1" class="text-center font-medium"
+          >Trial</label
+        >
+        <label v-else class="text-center font-medium">Trial {{ i }}</label>
+        <div class="flex justify-center space-x-2">
           <input
             type="radio"
             title="true"
             value="true"
-            v-model="data[i-1]"
-          >
-        </div>
-        <div class="px-2 scale-125">
+            v-model="data[i - 1]"
+            class="scale-125"
+          />
           <input
             type="radio"
             title="false"
             value="false"
-            v-model="data[i-1]"
-          >
+            v-model="data[i - 1]"
+            class="scale-125"
+          />
         </div>
       </div>
+      <div v-if="type === 'COUNT' || type === 'COUNT_ARRAY'" class="space-y-2">
+        <label v-if="verifiedCount === 1" class="text-center font-medium"
+          >Count</label
+        >
+        <label v-else class="text-center font-medium">Count {{ i }}</label>
+        <input
+          v-model.number="data[i - 1]"
+          class="w-24 text-black text-center border rounded p-1"
+        />
+      </div>
     </div>
-    <div v-if="type === 'COUNT' || type === 'COUNT_ARRAY'">
-      <label v-if="verifiedCount==1" class="flex justify-center">Count</label>
-      <label v-else class="flex justify-center">Count {{ i }}</label>
-      <input
-        v-model.number="data[i-1]"
-        class="w-24 text-black text-center"
-      >
-    </div>
-  </div>
   </div>
 </template>
