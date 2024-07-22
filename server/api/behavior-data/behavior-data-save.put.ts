c9a5bbd77731867ac0a3984a54a1b6ea31da29ae
Anusha <ahsuna.bajracharya@gmail.com>
2024-07-22 15:08:55 -0500

    updates submit api

--- server/api/behavior-data/behavior-data-save.put.ts
+++ server/api/behavior-data/behavior-data-save.put.ts
@@ -1,45 +1,76 @@
-import { PrismaClient } from "@prisma/client/extension";
-const prisma = new PrismaClient();
-
 export default defineEventHandler(async (event) => {
-  const { behaviorID, sessionID, data, type } = await readBody(event);
-  /*let convertedData;
-
-  if (type == "COUNT" || "TRIAL") {
-    convertedData = String(data);
-  } else if (type == "COUNTARRAY" || "TRIALARRAY") {
-    convertedData = "";
-    for (let i = 0; i < data.length; i++) {
-      convertedData = convertedData + String(data[i]) + ",";
-    }
-  }
-
-  const behaviorData = await event.context.prisma.behaviorData.findUnique({
+  const { behaviorID, sessionID, data, type, submitted } = await readBody(
+    event
+  );
+  const session = await event.context.prisma.session.findUnique({
     where: {
-      sessionId: Number(sessionID),
-      behaviorId: Number(behaviorID),
+      id: Number(sessionID),
     },
   });
 
-  if (behaviorData) {
-    await event.context.prisma.behaviorData.update({
+  if (!session.submitted) {
+    let convertedData;
+
+    if (type == "COUNT" || type == "TRIAL") {
+      convertedData = String(data);
+      return {};
+      /*return {
+        statusCode: 400,
+      };*/
+    } else if (type == "COUNTARRAY" || type == "TRIALARRAY") {
+      convertedData = "";
+      for (let i = 0; i < data.length; i++) {
+        convertedData = convertedData + String(data[i]) + ",";
+      }
+
+      /*return {
+        body: convertedData,
+        statusCode: 300,
+      };*/
+    }
+
+    const behaviorData = await event.context.prisma.behaviorData.findFirst({
       where: {
-        id: behaviorData.id,
-      },
-      data: {
-        data: convertedData,
-      },
-    });
-  } else {
-    await event.context.prisma.behaviorData.create({
-      data: {
         sessionId: Number(sessionID),
         behaviorId: Number(behaviorID),
-        data: convertedData,
       },
     });
-  }*/
 
+    if (behaviorData) {
+      await event.context.prisma.behaviorData.update({
+        where: {
+          id: behaviorData.id,
+        },
+        data: {
+          data: convertedData,
+        },
+      });
+    } else {
+      await event.context.prisma.behaviorData.create({
+        data: {
+          sessionId: Number(sessionID),
+          behaviorId: Number(behaviorID),
+          data: convertedData,
+        },
+      });
+    }
+  }
+
+  if (Boolean(submitted)) {
+    await event.context.prisma.session.update({
+      where: {
+        id: sessionID,
+      },
+      data: {
+        submitted: true,
+      },
+    });
+  }
+
+  /*return {
+    body: submitted,
+    statusCode: 300,
+  };*/
   return {
     statusCode: 200,
   };
