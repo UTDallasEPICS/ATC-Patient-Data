de31259949ad3bf86d3bcbbda32a85a3d59ac0b9
Emory Grubbs <104403182+emoryjgrubbs@users.noreply.github.com>
2024-07-25 13:52:02 -0500

    !Fix to creating a session

--- server/api/session/create.post.ts
+++ server/api/session/create.post.ts
@@ -20,6 +20,7 @@ export default defineEventHandler(async (event) => {
       studentId: studentProfile.id,
       employeeId: employeeProfile.id,
       note: "",
+      createdAt: new Date(),
     },
   });
 
